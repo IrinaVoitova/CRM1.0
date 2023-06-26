731bfe437fd0d5960396f2916e51797c398fc732
Irina Voitova <iravoitova1994@gmail.com>
2023-06-26 20:54:11 +0300

    init dev

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// в константы получите элементы из модального окна
+
+// 1. Заголовок 
+
+// 2. Кнопка возле id и сам id
+
+// 3. Форма
+
+// 4. Чекбокс и  Поле рядом с чекбоксом
+
+// 5. Итоговую стоимость
+
+const title = document.querySelector('.modal__label_name');
+console.log(title);
+
+const id = document.querySelector('.modal__id');
+console.log(id);
+
+const close = document.querySelector('.modal__btn_close');
+console.log(close);
+
+const form = document.querySelector('.modal__form')
+console.log(form);
+
+const discount = document.querySelector('.modal__input_discount');
+console.log(discount);
+
+const checkbox = document.querySelector('.modal__checkbox');
+console.log(checkbox);
+
+const total = document.querySelector('.add__p_span');
+console.log(total);
