1ea29e2f67ab9c08906048239b55be8287ff6f21
Irina Voitova <iravoitova1994@gmail.com>
2023-06-26 20:54:11 +0300

    удалены комментарии

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const title = document.querySelector('.modal__label_name');
+console.log(title);
+
+const id = document.querySelector('.modal__id');
+console.log(id);
+
+const close = document.querySelector('.modal__btn_close');
+console.log(close);
+
+const form = document.querySelector('.modal__form');
+console.log(form);
+
+const discount = document.querySelector('.modal__input_discount');
+console.log(discount);
+
+const checkbox = document.querySelector('.modal__checkbox');
+console.log(checkbox);
+
+const total = document.querySelector('.add__p_span');
+console.log(total);
