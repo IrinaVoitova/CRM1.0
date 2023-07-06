c3807a13733bf64d99237823a5c424d0228e11a4
Irina Voitova <iravoitova1994@gmail.com>
2023-07-06 15:18:38 +0300

    5.5

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
 'use strict';
 
-const title = document.querySelector('.modal__label_name');
-console.log(title);
+// const title = document.querySelector('.modal__label_name');
+// console.log(title);
 
-const id = document.querySelector('.modal__id');
-console.log(id);
+// const id = document.querySelector('.modal__id');
+// console.log(id);
 
-const close = document.querySelector('.modal__btn_close');
-console.log(close);
+// const close = document.querySelector('.modal__btn_close');
+// console.log(close);
 
-const form = document.querySelector('.modal__form');
-console.log(form);
+// const form = document.querySelector('.modal__form');
+// console.log(form);
 
-const discount = document.querySelector('.modal__input_discount');
-console.log(discount);
+// const discount = document.querySelector('.modal__input_discount');
+// console.log(discount);
 
-const checkbox = document.querySelector('.modal__checkbox');
-console.log(checkbox);
+// const checkbox = document.querySelector('.modal__checkbox');
+// console.log(checkbox);
 
-const total = document.querySelector('.add__p_span');
-console.log(total);
+// const total = document.querySelector('.add__p_span');
+// console.log(total);
+
+
+
+const openModal = () => {
+    const btnAdd = document.querySelector('.table__add');
+    const overlay = document.querySelector('.overlay');
+    const btnExit = document.querySelector('.modal__btn_close');
+
+    const startModal = () => {
+        overlay.classList.add('startModal')
+    };
+
+    const exitModal = () => {
+        overlay.classList.remove('startModal')
+    };
+
+    btnAdd.addEventListener('click', startModal);
+    btnExit.addEventListener('click', exitModal);
+};
+
+openModal();
